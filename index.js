b0b598604352e9d98959bf53460f6d11b725cc4d
f0rx4 <34268827+f0rx4@users.noreply.github.com>
2020-06-27 10:50:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ if (process.env.HEROKU_APP_NAME) {
 				}
 			}
 		)
-		setTimeout(preventIdling, 5 * 60 * 1000) // 5 min
+		setTimeout(preventIdling, 45 * 60 * 1000) // 45 min
 	}
 	preventIdling()
 }
